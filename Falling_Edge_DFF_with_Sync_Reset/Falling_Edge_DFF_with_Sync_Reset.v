2f3a81ea84608b88248eecbdc5ae9705689dbe44
Süleyman Yasin Peker <68354896+yasin-peker@users.noreply.github.com>
2023-08-09 21:01:40 +0300

    Add files via upload

---
+++ Falling_Edge_DFF_with_Sync_Reset/Falling_Edge_DFF_with_Sync_Reset.v
@@ -0,0 +1,21 @@
+module Falling_Edge_DFF_with_Sync_Reset(
+	input D,
+	input clk,
+	input Sync_Reset,
+	output reg Q
+);
+
+always @(negedge clk)
+begin
+
+	if(Sync_Reset == 1'b1)
+		
+		Q <= 1'b0;
+	
+	else
+	
+		Q <= D;
+	
+end
+
+endmodule
